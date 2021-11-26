f08efc4d574e1606b0acab0b073833f466c3d4f5
AnaComBia <anabeatrizetorres@gmail.com>
2021-11-26 20:11:01 -0300

    finaliza programa SequênciaLimites

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SequenciaLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---Sequência Limites---");
+
+            Console.Write("\nMe fale o número em que a contagem irá iniciar: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Me fale o número em que a contagem irá acabar: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            do
+            {
+
+                Console.WriteLine($"\n{number1}");
+
+                number1 = number1 + 1;
+
+            }
+            while (number1 <= number2);
+
+
+
+        }
+    }
+}
